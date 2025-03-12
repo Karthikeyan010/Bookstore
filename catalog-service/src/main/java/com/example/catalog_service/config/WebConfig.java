05a2538b11f36cafbf2ff4846027cd1d6d70b74a
Ayah Arafat <A.Arafat2@newcastle.ac.uk>
2025-03-12 23:37:01 +0000

    try to remove config, as we do not use security

--- catalog-service/src/main/java/com/example/catalog_service/config/WebConfig.java
+++ catalog-service/src/main/java/com/example/catalog_service/config/WebConfig.java
@@ -5,15 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+/*@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("*")
-                .allowedMethods("*")   
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
-}
+}*/
