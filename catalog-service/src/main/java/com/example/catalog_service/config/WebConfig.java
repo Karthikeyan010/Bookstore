c7dee468ba761497bbef7774112bcbc3e9193dbc
Ayah Arafat <A.Arafat2@newcastle.ac.uk>
2025-03-12 21:50:41 +0000

    fix config file to allow all origins

--- catalog-service/src/main/java/com/example/catalog_service/config/WebConfig.java
+++ catalog-service/src/main/java/com/example/catalog_service/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/catalog")
-                .allowedOrigins("http://34.147.185.149:8080") // Allow frontend URL
-                .allowedMethods("GET"); // Allow GET request
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("*");
     }
 }
