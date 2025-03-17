8092b820ef12674b66cd6e0bdb326327e23f280f
Ayah Arafat <A.Arafat2@newcastle.ac.uk>
2025-03-17 01:11:18 +0000

    test

--- catalog-service/src/main/java/com/example/catalog_service/controller/BookController.java
+++ catalog-service/src/main/java/com/example/catalog_service/controller/BookController.java
@@ -26,7 +26,7 @@ public class BookController {
 
     @GetMapping
     public List<Book> getAllBooks() {
-        logger.info("Request received to get all books-testrolloutnew0000000000012345");
+        logger.info("Request received to get all books-testrolloutnew0000000000015552345");
         List<Book> books = bookService.getAllBooks();
         logger.info("Returning {} books", books.size());
         return books;
