bbeb846164b3be23e964d770cf721f7b1e8fc709
Ayah Arafat <A.Arafat2@newcastle.ac.uk>
2025-03-18 22:55:11 +0000

    trigger pipelines

--- catalog-service/src/main/java/com/example/catalog_service/controller/BookController.java
+++ catalog-service/src/main/java/com/example/catalog_service/controller/BookController.java
@@ -28,7 +28,7 @@ public class BookController {
 
     @GetMapping
     public List<Book> getAllBooks() {
-        logger.info("Request received to get all books-testrolloutnew12345678912");
+        logger.info("Request received to get all books-testrolloutnew123");
         List<Book> books = bookService.getAllBooks();
         logger.info("Returning {} books", books.size());
         return books;
